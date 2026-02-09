406f771cf13383ea21bd3a5fc71b214a8e8c5ccd
Krystian <krystian@mac.home>
2026-02-09 23:26:27 +0100

    first commit

---
+++ lua/plugins/plugins.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    opts = {},
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        -- Automatically install the following LSPs
+        ensure_installed = {
+          "lua_ls",
+          "rust_analyzer",
+          "clangd",
+          "eslint",
+          "pylsp",
+          "gopls",
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+  },
+}
